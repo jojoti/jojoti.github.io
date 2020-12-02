31ac2d1c3356c65494efd41f3063d055c985a8b7
panyao <pandaxiawork@outlook.com>
2020-12-02 20:10:34 +0800

    fix: js 多语言配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,7 @@
 module.exports = {
     title: 'JoJo王',
-    description: 'Just playing around'
+    description: '记录的地方',
+    // https://vuepress.vuejs.org/zh/theme/default-theme-config.html#%E5%AF%BC%E8%88%AA%E6%A0%8F-logo
+
+
 }
